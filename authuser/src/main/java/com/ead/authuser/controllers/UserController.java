603162621b9924129af637c045397989649ce072
Matheus <mbwolney26@gmail.com>
2021-12-05 23:31:44 -0300

    Validação nos usuários

--- authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -55,7 +56,8 @@ public class UserController {
     // Atualizar usuário
     @PutMapping("/{userId}")
     public ResponseEntity<Object> updateUser(@PathVariable(value = "userId") UUID userId,
-                                             @RequestBody @JsonView(UserDto.UserView.UserPut.class) UserDto userDto){
+                                             @RequestBody @Validated(UserDto.UserView.UserPut.class)
+                                             @JsonView(UserDto.UserView.UserPut.class) UserDto userDto){
         Optional<UserModel> userModelOptional = userService.findById(userId);
 
         if(!userModelOptional.isPresent()){ //Verifica se o usuário existe
@@ -74,7 +76,8 @@ public class UserController {
     // Atualizar senha
     @PutMapping("/{userId}/password")
     public ResponseEntity<Object> updatePassword (@PathVariable(value = "userId") UUID userId,
-                                                  @RequestBody @JsonView(UserDto.UserView.PasswordPut.class) UserDto userDto) {
+                                                  @RequestBody @Validated(UserDto.UserView.PasswordPut.class)
+                                                  @JsonView(UserDto.UserView.PasswordPut.class) UserDto userDto) {
         Optional<UserModel> userModelOptional = userService.findById(userId);
 
         if (!userModelOptional.isPresent()) { //Verifica se o usuário existe
@@ -94,7 +97,8 @@ public class UserController {
     // Atualizar ImageUrl
     @PutMapping("/{userId}/image")
     public ResponseEntity<Object> updateImage (@PathVariable(value = "userId") UUID userId,
-                                               @RequestBody @JsonView(UserDto.UserView.ImagePut.class) UserDto userDto) {
+                                               @RequestBody @Validated(UserDto.UserView.ImagePut.class)
+                                               @JsonView(UserDto.UserView.ImagePut.class) UserDto userDto) {
         Optional<UserModel> userModelOptional = userService.findById(userId);
 
         if (!userModelOptional.isPresent()) { //Verifica se o usuário existe
