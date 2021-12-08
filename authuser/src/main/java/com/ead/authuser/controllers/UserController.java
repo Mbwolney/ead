df955274cc933721c80f87b6206accef2ae713e1
Matheus <mbwolney26@gmail.com>
2021-12-07 21:15:28 -0300

    Filtro(Specification)

--- authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.ead.authuser.controllers;
 import com.ead.authuser.dtos.UserDto;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.service.UserService;
+import com.ead.authuser.specifications.SpecificationTemplate;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -28,10 +29,12 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    // Consulta todos os usuários, passando O filtro(Specification) e a Paginação
     @GetMapping
-    public ResponseEntity<Page<UserModel>> getAllUsers(@PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) // Paginação (page = 0 -> Pagina zero / sort = 10 -> É quant. de elem. por pagina / userId do menor id para o maior)
+    public ResponseEntity<Page<UserModel>> getAllUsers( SpecificationTemplate.UserSpec spec,
+                                                        @PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) // Paginação (page = 0 -> Pagina zero / sort = 10 -> É quant. de elem. por pagina / userId do menor id para o maior)
                                                                    Pageable pageable){
-        Page<UserModel> userModelPage = userService.findAll(pageable);
+        Page<UserModel> userModelPage = userService.findAll(spec, pageable);
         return ResponseEntity.status(HttpStatus.OK).body(userModelPage);
     }
 
---
+++ authuser/src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
@@ -0,0 +1,21 @@
+package com.ead.authuser.specifications;
+
+import com.ead.authuser.models.UserModel;
+import net.kaczmarzyk.spring.data.jpa.domain.Equal;
+import net.kaczmarzyk.spring.data.jpa.domain.Like;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import org.springframework.data.jpa.domain.Specification;
+
+public class SpecificationTemplate {
+
+    // FILTRO
+    @And({
+            @Spec(path = "userType", spec = Equal.class), // O path é o atributo que vai filtrar, spec é o tipo que vai utilizar na Specification do tipo EQUAL
+            @Spec(path = "userStatus", spec = Equal.class),
+            @Spec(path = "email", spec = Like.class) // LIKE é o tipo igual do SQL %Math%
+    })
+    public interface UserSpec extends Specification<UserModel> {
+
+    }
+}
