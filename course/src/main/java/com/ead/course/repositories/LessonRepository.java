421de99968401435aa2b6f728a2d00c8a75885e8
Matheus <mbwolney26@gmail.com>
2021-12-21 10:16:33 -0300

    CRUD de Lessons

--- course/src/main/java/com/ead/course/repositories/LessonRepository.java
+++ course/src/main/java/com/ead/course/repositories/LessonRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface LessonRepository extends JpaRepository<LessonModel, UUID> {
@@ -14,4 +15,7 @@ public interface LessonRepository extends JpaRepository<LessonModel, UUID> {
     @Modifying // Com essa anotação vou poder atualizar e deletar
     @Query(value = "SELECT * FROM tb_lessons WHERE module_module_id = :moduleId", nativeQuery = true)
     List<LessonModel> findAllLessonsInToModule(@Param("moduleId") UUID moduleId);
+
+    @Query(value = "SELECT * FROM tb_lessons WHERE module_module_id = :moduleId AND lesson_id = :lessonId", nativeQuery = true)
+    Optional<LessonModel> findLessonIntoModule(@Param("moduleId") UUID moduleId, @Param("lessonId") UUID lessonId);
 }
