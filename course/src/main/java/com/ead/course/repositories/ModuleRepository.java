21af4e3086e1787008319a01d4ee561679219fc3
Matheus <mbwolney26@gmail.com>
2021-12-21 16:00:59 -0300

    Criando paginação e filtros nos course, modules e lessons

--- course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -2,7 +2,7 @@ package com.ead.course.repositories;
 
 import com.ead.course.models.ModuleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ModuleRepository  extends JpaRepository<ModuleModel, UUID> {
+public interface ModuleRepository  extends JpaRepository<ModuleModel, UUID>, JpaSpecificationExecutor<ModuleModel> {
 
 //    @Modifying // Com essa anotação vou poder atualizar e deletar
     @Query(value = "SELECT * FROM tb_modules WHERE course_course_id = :courseId", nativeQuery = true)
