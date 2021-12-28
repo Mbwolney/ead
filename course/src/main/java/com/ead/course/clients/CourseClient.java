946a34a51be6787c3157ada53a52b94ab6f73b81
Matheus <mbwolney26@gmail.com>
2021-12-28 12:09:58 -0300

    Comunicação sincrona User com Course

--- course/src/main/java/com/ead/course/clients/CourseClient.java
+++ course/src/main/java/com/ead/course/clients/CourseClient.java
@@ -1,8 +1,8 @@
-package com.ead.authuser.clients;
+package com.ead.course.clients;
 
-import com.ead.authuser.dtos.CourseDto;
-import com.ead.authuser.dtos.ResponsePageDto;
-import com.ead.authuser.service.UtilsService;
+import com.ead.course.dtos.ResponsePageDto;
+import com.ead.course.dtos.UserDto;
+import com.ead.course.services.UtilsService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
@@ -19,7 +19,7 @@ import java.util.UUID;
 
 @Log4j2
 @Component
-public class UserClient {
+public class CourseClient {
 
     @Autowired
     RestTemplate restTemplate;
@@ -27,23 +27,23 @@ public class UserClient {
     @Autowired
     UtilsService utilsService;
 
-    String REQUEST_URI = "http://localhost:8082";
+    String REQUEST_URI = "http://localhost:8087";
 
-    public Page<CourseDto> getAllCoursesByUsers(UUID userId, Pageable pageable){
-        List<CourseDto> searchResult = null;
-        String url = utilsService.createUrl(userId, pageable);
+    public Page<UserDto> getAllUsersByCourse(UUID courseId, Pageable pageable){
+        List<UserDto> searchResult = null;
+        String url = utilsService.createUrl(courseId, pageable);
         log.debug("Request URL: {} ", url);
         log.info("Request URL: {} ", url);
         try{
-            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
-            ResponseEntity<ResponsePageDto<CourseDto>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            ParameterizedTypeReference<ResponsePageDto<UserDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<UserDto>>() {};
+            ResponseEntity<ResponsePageDto<UserDto>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
             searchResult = result.getBody().getContent();
 
             log.debug("Reponse Number of Elements: {} ", searchResult.size());
         } catch (Exception e){
             log.error("Error request /courses {} ", e);
         }
-        log.info("Ending request /courses userId {} ", userId);
+        log.info("Ending request /users courseId {} ", courseId);
         return new PageImpl<>(searchResult);
     }
 }
